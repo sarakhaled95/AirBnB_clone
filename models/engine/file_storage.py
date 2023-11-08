327b30a7e9a5fefe772f84288a6d9d399ed152b9
sarakhaled95 <architectsarakhaled@outlook.com>
2023-11-08 07:10:02 -0800

    saving

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,4 +5,21 @@ import json
 
 class FileStorage:
     """class that serializes instances to a JSON file and 
-    deserializes JSON file to instances"
+    deserializes JSON file to instances"""
+    __file_path: str = "file.json"
+    __objects: dict = {}
+
+    def __init__(self):
+        """constructor"""
+        pass
+
+    def all(self):
+        """returns the dictionary"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+
