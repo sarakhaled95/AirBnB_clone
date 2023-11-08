8cb1c52e1ce0f9e14a84fa7fef8eb53ce32a4ed6
sarakhaled95 <architectsarakhaled@outlook.com>
2023-11-08 06:09:00 -0800

    saving

---
+++ models/engine/file_storage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""FileStorage class module"""
+import json
+
+
+class FileStorage:
+    """class that serializes instances to a JSON file and 
+    deserializes JSON file to instances"
