605c7fbcce54fc221c4a41452ac0fb6e9e175720
sarakhaled95 <architectsarakhaled@outlook.com>
2023-11-08 03:07:19 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,19 @@ class BaseModel:
     def __init__(self):
         """constructor method"""
         self.id = str(uuid.uuid4())
-        self.create_at = datetime.now()
+        self.create_at = datetime.now().isoformat()
         self.update_at = self.create_at
 
-    def update(self):
+    def __str__(self):
+        """string representation od class name and id"""
+        return '[{}] ({}) {}'.\
+                format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
         """it will be updated every time you change your object"""
-        self.update_at = datetime.now()
+        self.update_at = datetime.now().isoformat()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values"""
+        my_dict = self.__dict__
+        my_dict['__class__'] = type(self.__name__)
