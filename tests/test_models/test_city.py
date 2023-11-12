7a126c7bccbeab6d4dbaa3f91c6b65b085fcc88e
sarakhaled95 <architectsarakhaled@outlook.com>
2023-11-12 08:02:42 -0800

    city tests

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""unittest for city class module"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.city import City
+
+
+class TestCity_inst(unittest.TestCase):
+    """unittesting for instantiation of the City class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(City, type(City()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(City(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(City().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(City().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(City().updated_at))
+
+    def test_state_id_is_public_class_attribute(self):
+        cy = City()
+        self.assertEqual(str, type(City.state_id))
+        self.assertIn("state_id", dir(cy))
+        self.assertNotIn("state_id", cy.__dict__)
+
+    def test_name_is_public_class_attribute(self):
+        cy = City()
+        self.assertEqual(str, type(City.name))
+        self.assertIn("name", dir(cy))
+        self.assertNotIn("name", cy.__dict__)
+
+    def test_two_cities_unique_ids(self):
+        cy1 = City()
+        cy2 = City()
+        self.assertNotEqual(cy1.id, cy2.id)
+
+    def test_diff_created_at(self):
+        cy1 = City()
+        sleep(0.05)
+        cy2 = City()
+        self.assertLess(cy1.created_at, cy2.created_at)
+
+    def test_diff_updated_at(self):
+        cy1 = City()
+        sleep(0.05)
+        cy2 = City()
+        self.assertLess(cy1.updated_at, cy2.updated_at)
+
+
+if __name__ == "__main__":
+    unittest.main()
